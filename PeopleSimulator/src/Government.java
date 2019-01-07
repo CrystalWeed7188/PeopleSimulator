8e31981b8c6854fe029dab4396d6a5f8edfd1830
CrystalWeed7188 <crystalweed7188@gmail.com>
2019-01-07 15:37:05 +0000

    Deleted everything other than taxes, changed taxation

--- PeopleSimulator/src/Government.java
+++ PeopleSimulator/src/Government.java
@@ -1,36 +1,17 @@
 class Government{
 //initialized variables
 private double taxRate;
-private int militarySpending;
-private int educationSpending;
-private int healthCareSpending;
-private int socialSecuritySpending;
 private double federalReserve;
-private int miscellaneousSpending;
 
 
 //constructer
-public Government(double taxRate, int militarySpending, int educationSpending, int healthCareSpending, int socialSecuritySpending, int miscellaneousSpending){
+public Government(double taxRate){
 this.taxRate = taxRate;
-this.setMilitarySpending(militarySpending);
-this.setEducationSpending(educationSpending);
-this.setHealthCareSpending(healthCareSpending);
-this.setSocialSecuritySpending(socialSecuritySpending);
-this.setMiscellaneousSpending(miscellaneousSpending);
 setFederalReserve(169000000);
 }
 
 
-
 //getters and setters
-
-public int getMiscellaneousSpending() {
-	return miscellaneousSpending;
-}
-
-public void setMiscellaneousSpending(int miscellaneousSpending) {
-	this.miscellaneousSpending = miscellaneousSpending;
-}
 
 public double getFederalReserve() {
 	return federalReserve;
@@ -38,38 +19,6 @@ public double getFederalReserve() {
 
 public void setFederalReserve(double federalReserve) {
 	this.federalReserve = federalReserve;
-}
-
-public int getSocialSecuritySpending() {
-	return socialSecuritySpending;
-}
-
-public void setSocialSecuritySpending(int socialSecuritySpending) {
-	this.socialSecuritySpending = socialSecuritySpending;
-}
-
-public int getHealthCareSpending() {
-	return healthCareSpending;
-}
-
-public void setHealthCareSpending(int healthCareSpending) {
-	this.healthCareSpending = healthCareSpending;
-}
-
-public int getEducationSpending() {
-	return educationSpending;
-}
-
-public void setEducationSpending(int educationSpending) {
-	this.educationSpending = educationSpending;
-}
-
-public int getMilitarySpending() {
-	return militarySpending;
-}
-
-public void setMilitarySpending(int militarySpending) {
-	this.militarySpending = militarySpending;
 }
 
 public double getTaxRate() {
@@ -82,27 +31,27 @@ public void setTaxRate(double taxRate) {
 
 
 //methods
-public void taxAssignment(double income){
+public void taxAssignment(double income, double taxRateMultiplier){
 
 //will I make different tax rates for income levels? Probably account for single and married?
 // cycles money to federal federalReserve
-//Idk how taxes actually work btw
+//Idk how taxes actually work btw so its a progressive tax
 
     if(income > 0 &&  income < 10000){
-        taxRate = .10;
-        federalReserve += income*.1;
+        taxRateMultiplier = taxRate + .005;
+        federalReserve += income*taxRateMultiplier;
 
     }else if (income > 10000 &&  income < 38000){
-        taxRate = .12;
-        federalReserve += income*.12;
+        taxRateMultiplier = taxRate + .01;
+        federalReserve += income*taxRateMultiplier;
 
     } else if(income > 38000 &&  income < 82000){
-        taxRate = .22;
-        federalReserve += income*.22;
+         taxRateMultiplier = taxRate + .015;
+        federalReserve += income*taxRateMultiplier;
 
     } else{
-        taxRate = .30;
-        federalReserve += income*.3;
+         taxRateMultiplier = taxRate + .02;
+        federalReserve += income*taxRateMultiplier;
     }
 
   }
